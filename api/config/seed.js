b705c24f4254a772b979753f6beebb3e5e50b166
monja119 <monja.sesame@gmail.com>
2024-06-25 00:46:08 +0300

    update

--- api/config/seed.js
+++ api/config/seed.js
@@ -1,15 +1,63 @@
-const sqlite3 = require('sqlite3').verbose();
-const path = require('path');
+const db = require('./db');
 
-const dbPath = path.resolve(__dirname, 'blog.db');
-const db = new sqlite3.Database(dbPath);
+const seedDatabase = () => {
+    // creating database
+    const query = `
+        CREATE DATABASE IF NOT EXISTS zereko
+    `;
+    db.query(query, (err) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log('Database created successfully');
+    });
+}
+const seedUsers = () => {
+    // creating table
+    const query = `
+        CREATE TABLE IF NOT EXISTS users (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            name VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            imageUrl VARCHAR(255) NULL
+        )
+    `;
+};
 
-db.serialize(() => {
-    // Insérer les comptes administrateurs
-    db.run('INSERT INTO admin (login, pass) VALUES (?, ?)', ['admin1', 'password1']);
-    db.run('INSERT INTO admin (login, pass) VALUES (?, ?)', ['admin2', 'password2']);
+const seedProjects = () => {
+    // creating table
+    const query = `
+        CREATE TABLE IF NOT EXISTS projects (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            title VARCHAR(255) NOT NULL,
+            description TEXT NOT NULL,
+            date_debut DATE NOT NULL,
+            date_fin DATE NOT NULL,
+            created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        )
+    `;
+};
 
-    console.log('Seeders exécutés avec succès');
-});
+const seedMembers = () => {
+    // creating table
+    const query = `
+        CREATE TABLE IF NOT EXISTS members (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            project_id INT NOT NULL,
+            user_id INT NOT NULL,
+            role VARCHAR(255) NOT NULL,
+            created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        )
+    `;
+}
 
-db.close();
+const runSeeders = async () => {
+    await seedDatabase();
+    await seedUsers();
+    await seedProjects();
+    await seedMembers();
+};
+
+runSeeders();
